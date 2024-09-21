eee9e3dd0ef1fc600497dc88a2c5a95943a50336
Paha3555 <sbot08016@gmail.com>
2024-09-21 16:53:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put session id",
+SESSION_ID: process.env.SESSION_ID || "ljczHLzB#A-1nZv-jgAUG9zJsrEpUHssGHPERFAjlNG-nxjLm9YA",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/FW2r9Xh/IMG-20240921-WA0114.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "𝐇𝐄𝐋𝐋𝐎 👋 ${pushname} 𝗤𝗨𝗘𝗘𝗡-𝗖𝗛𝗢𝗢𝗧𝗬-𝗡𝗘𝗟𝗨𝗠𝗜-𝗠𝗗 Whatsapp Bot 💗👨‍💻",
 SUDO_NB: process.env.SUDO_NB || "94757286833",
