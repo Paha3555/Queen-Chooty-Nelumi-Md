5857c5d9505be449c322ebd5a0d1ec3170e8d6ae
Navinofc44 <Pahasarasanujja@gmail.com>
2024-09-21 16:29:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put sesson id",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://imgtr.ee/image/IMG-20240917-WA0492.hdhlvD",
+SESSION_ID: process.env.SESSION_ID || "put session id",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/FW2r9Xh/IMG-20240921-WA0114.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "𝐇𝐄𝐋𝐋𝐎 👋 ${pushname} 𝗤𝗨𝗘𝗘𝗡-𝗖𝗛𝗢𝗢𝗧𝗬-𝗡𝗘𝗟𝗨𝗠𝗜-𝗠𝗗 Whatsapp Bot 💗👨‍💻",
 SUDO_NB: process.env.SUDO_NB || "94757286833",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
