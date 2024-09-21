3fd6bd28893d159587f9fe035797bbba3de1ee25
Navinofc44 <Pahasarasanujja@gmail.com>
2024-09-21 15:01:06 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "put sesson id",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://imgtr.ee/image/IMG-20240917-WA0492.hdhlvD",
+ALIVE_MSG: process.env.ALIVE_MSG || "𝐇𝐄𝐋𝐋𝐎 👋 ${pushname} 𝗤𝗨𝗘𝗘𝗡-𝗖𝗛𝗢𝗢𝗧𝗬-𝗡𝗘𝗟𝗨𝗠𝗜-𝗠𝗗 Whatsapp Bot 💗👨‍💻",
+SUDO_NB: process.env.SUDO_NB || "94757286833",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "public",
+AUTO_VOICE:"true"
+};
+
